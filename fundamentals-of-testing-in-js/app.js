36e71a3723947645303314d27d5d38fc442213e0
Shehroz Irfan <shehrozirfan89@gmail.com>
2023-02-09 12:47:28 +0500

    [Fundamentals of testing in js] Abstract test assertions

--- fundamentals-of-testing-in-js/app.js
+++ fundamentals-of-testing-in-js/app.js
@@ -1,11 +1,17 @@
-//Throw an error with a simple test
-
 const sum = (a, b) => a + b;
 const sub = (a, b) => a - b;
 
 const result = sum(7, 3);
 const expected = 9;
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`);
+      }
+    },
+  };
 }
+
+expect(result).toBe(expected);
