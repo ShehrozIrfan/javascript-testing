731c86442877598cd43cdad5f7e259b93b53a553
Shehroz Irfan <shehrozirfan89@gmail.com>
2023-02-09 13:01:32 +0500

    [Fundamentals of testing in js] Encapsulates and isolate thhe tests

--- fundamentals-of-testing-in-js/app.js
+++ fundamentals-of-testing-in-js/app.js
@@ -1,17 +1,40 @@
+//Function to sum, sub the numbers
 const sum = (a, b) => a + b;
 const sub = (a, b) => a - b;
 
-const result = sum(7, 3);
-const expected = 9;
-
+//function to expect the result with expected value
 function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${result} is not equal to ${expected}`);
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
     },
   };
 }
 
-expect(result).toBe(expected);
+//function to display the information related to tests
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✓ ${title}`);
+  } catch (error) {
+    console.error(`✕ ${title}`);
+    console.error(error);
+  }
+}
+
+//performing the tests
+test("sum add the numbers", () => {
+  const result = sum(7, 3);
+  const expected = 10;
+
+  expect(result).toBe(expected);
+});
+
+test("sub minus the numbers", () => {
+  const result = sub(7, 3);
+  const expected = 4;
+
+  expect(result).toBe(expected);
+});
